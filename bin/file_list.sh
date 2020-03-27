d6062fef2218c1885717d82e903abc522b687660
Brandon Shega <b.shega@gmail.com>
2020-03-27 17:00:22 -0400

    Working on tmux stuff

--- bin/file_list.sh
+++ bin/file_list.sh
@@ -10,9 +10,9 @@ if [ -f $manualfile ]; then
   files="$(cat $manualfile)"
 else
   # Get list of files to link
-  includes=".vim .zsh .mutt .git_template .config .tmux .spacemacs.d .doom.d .SpaceVim.d"
+  includes=".vim .zsh .mutt .git_template .config .spacemacs.d .doom.d .SpaceVim.d"
   excludes=".gitignore"
-  base="$(find . -maxdepth 1 -name '.*' -not -name '.*.local' -not -name '*.swp' -type f | sed 's#^\./##' | grep -vF $excludes)" files="$base $includes"
+  base="$(find -L . -maxdepth 1 -name '.*' -not -name '*.swp' -type f | sed 's#^\./##' | grep -vF $excludes)" files="$base $includes"
 fi
 
 echo $files
